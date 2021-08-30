3e6934843e454c2fc123eefa0bc007887c067159
Denis Rampnoux <troisdiz@users.noreply.github.com>
2021-08-30 23:15:05 +0200

    Code skeleton + update requirements.txt

--- pmfp/main.py
+++ pmfp/main.py
@@ -1,3 +1,24 @@
-if __name__ == '__main__':
-    print("Hello")
+import threading
+import time
+from datetime import datetime
 
+import schedule
+
+
+def job():
+    print("Start Job at : " + str(datetime.now()))
+    print("Do something")
+    print("End Job at   : " + str(datetime.now()))
+
+
+def run_threaded(job_func):
+    job_thread = threading.Thread(target=job_func)
+    job_thread.start()
+
+
+if __name__ == '__main__':
+
+    schedule.every().minute.at(":35").do(run_threaded, job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
