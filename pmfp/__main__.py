5c3cb1651ba01ecea653c5aedc90c91c535687a3
Denis Rampnoux <troisdiz@users.noreply.github.com>
2021-09-23 23:52:38 +0200

    Setup argparse

--- pmfp/__main__.py
+++ pmfp/__main__.py
@@ -1,4 +1,6 @@
+import argparse
 import threading
+import sys
 import time
 from datetime import datetime
 
@@ -16,8 +18,39 @@ def run_threaded(job_func):
     job_thread.start()
 
 
+class CliOptions:
+    def __init__(self, ask_version: bool, ask_help: bool):
+        self.ask_version: bool = ask_version
+        self.ask_help: bool = ask_help
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser(prog='pmfp')
+    # version
+    parser.add_argument(
+        "-v",
+        "--version",
+        dest="ask_version",
+        help="Show version and exit",
+        action="store_true",
+    )
+    # config file path
+    # log path?
+    args = parser.parse_args()
+    return CliOptions(args.ask_version, True)
+
+
 def main():
+    cli_options = _parse_args()
+    if cli_options.ask_version:
+        print('pmfp version 0.0.1')
+        sys.exit(0)
+
     schedule.every().minute.at(":35").do(run_threaded, job)
     while True:
         schedule.run_pending()
         time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
